af3f523329ad3bff73097a68fe2b80ef4ce5a4c4
Gabizzix <gabriela2mattos.5@gmail.com>
2023-11-27 18:33:52 -0400

    Trabalho Final PTAC

---
+++ src/Home/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home(){
+    return(
+        <center><h1>MEGAFLIX</h1></center>
+     );
+}
---
+++ src/cadastro.jsx
@@ -0,0 +1,2 @@
+const nome = prompt("Bem-vindo(a) ao Megaflix, qual é seu nome?");
+document.write("Bom jogo, " + nome);
